e2c05251c8815647839d1353865fc02d687b468b
Tom Harris <tom_3rd_2000@yahoo.com>
2020-09-03 06:45:15 -0400

    adding react styling to my components, src/App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, {useState, useEffect} from "react";
 import "./App.css";
 import axios from 'axios'
 import Card from './Card'
-import styled from "styled-components";
 import MyButton from './myButton'
 
 
