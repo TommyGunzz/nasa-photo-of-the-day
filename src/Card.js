17736c105434896f7f8794097b32d1c809245c48
Tom Harris <tom_3rd_2000@yahoo.com>
2020-09-03 06:45:48 -0400

    adding react styling to my components, src/Card.js

--- src/Card.js
+++ src/Card.js
@@ -1,16 +1,29 @@
 import React from 'react'
+import styled from "styled-components";
+
+const CardStyle = styled.div`
+color: black;
+margin: 10px;
+padding: 10px;
+background-color: turquoise;
+border-radius: 15px;
+width: 100%;
+transition: height 0.5s;
+`;
+
 
 function Card(props){
     console.log('props:', props)
     return (
+        <CardStyle>
     <div>
         <h1>{props.data.title}</h1>
         <p>{props.data.date}</p>
         <p>{props.data.explanation}</p>
         <footer>{props.data.copyright}</footer>
         
-        </div>
-
+    </div>
+        </CardStyle>
     )
 }
 
